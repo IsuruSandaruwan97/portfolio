bcee973ca82d40412d6a8a70bf19a7f30cd91cad
IsuruSandaruwan97 <isuruskarunarathne@gmail.com>
2024-07-24 17:52:21 +0530

    Add coming-soon UI

---
+++ src/features/coming-soon/index.svelte
@@ -0,0 +1,6 @@
+<div
+	class="flex flex-col h-full align-middle text-center self-center justify-center content-center"
+>
+	<h1 class="text-6xl">Coming Soon..!!</h1>
+	<p>We are currently working on creating something fantastic</p>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import Navbar from '../components/navbar/index.svelte';
-	import Home from '../features/home/index.svelte';
+	import ComingSoon from '../features/coming-soon/index.svelte';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -9,6 +8,6 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<Navbar />
-<Home />
+<ComingSoon />
+
 <slot />
